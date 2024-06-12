74b526b6ef0d35cdae7dd6443a26e41136455371
Sergey9030 <709030@mail.ru>
2024-06-12 19:26:07 +0300

    Add files via upload

---
+++ Lesson1.py
@@ -0,0 +1,33 @@
+# Длина строки
+a = 'lenght of string'
+print('Длина строки a"', a, '"=', len(a), 'символов')
+print()
+# Сумма и разность
+first = 5
+second = 7
+summa = first+second
+diff = second-first
+print('first=', first, '; second=', second)
+print('Сумма=', summa, '; Разность=', diff, 'или', first-second)
+print()
+# Среднее арифметическое
+third = 3
+mean = (first+second+third)/3
+print('first=', first, '; second=', second, '; third=', third)
+print('Среднее арифметическое=', mean)
+print()
+# Строки
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string+', '+first_string)
+print()
+# "Сложная" формула
+a = 1
+b = 2
+c = 3
+print('a=', a, '; b=', b, '; c=', c)
+f = (a*b)+(a*c)
+f = f**3
+f = f/2
+print('f=', f)
+print((((a*b)+(a*c))**3)/2)

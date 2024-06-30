af2773787c0f16303f0d4380d03157abda0b9f63
Sergey9030 <709030@mail.ru>
2024-06-30 15:09:23 +0300

    Add files via upload

---
+++ module_3_3.py
@@ -0,0 +1,12 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+print_params()
+print_params('1', 2, 3)
+print_params(b=5)
+values_list = [1, 'string', False]
+values_dict = {'a': 1, 'b': 'string', 'c': False}
+print_params(values_list)
+print_params(*values_list)
+print_params(values_dict)
+print_params(*values_dict)
+print_params(**values_dict)
